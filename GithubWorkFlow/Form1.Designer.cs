3655a2f2054766144c19323ececa67f8413a3d91
wbskyboy <wbskyboy@126.com>
2025-05-20 16:30:25 +0800

    1.给APp增加背景色

--- GithubWorkFlow/Form1.Designer.cs
+++ GithubWorkFlow/Form1.Designer.cs
@@ -47,6 +47,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
 			this.ClientSize = new System.Drawing.Size(800, 450);
 			this.Controls.Add(this.button1);
 			this.Name = "Form1";
